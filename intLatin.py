8fe53068f9a5863495cb5404f399df2e627394a7
romanos98d <35623989+romanos98d@users.noreply.github.com>
2018-02-16 14:05:54 +0200

    Function missing, rest is complete

--- intLatin.py
+++ intLatin.py
@@ -1,5 +1,22 @@
-x = input("Give an integer: ")
-while not isinstance(x, int):
-	x = input("I asked for an integer...: ")
+def partchecker(latin, remainder, n):
 
-	
+
+#integer input and check
+
+x = input("Give an integer between 1-1000000: ")
+while (not (isinstance(x, int)) or x > 1000000 or x < 1):
+	x = input("I asked for an integer between 1-1000000...: ")
+
+#latin array
+
+latin = ["[C][M]", "[D]", "[C][D]", "[C]", "[X][C]", "[L]", "[X][L]", "[X]", "M[X]", "[V]", "M[V]", "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
+
+#conversion process
+
+if (x == 1000000):
+	print("[M]")
+else:
+	for i in xrange(6, 0, -1):
+		x = partchecker(latin, x, i)
+
