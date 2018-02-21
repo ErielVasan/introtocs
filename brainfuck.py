f60d1959c22c4dc843bf2fbec7927e4429095d9d
romanos98d <35623989+romanos98d@users.noreply.github.com>
2018-02-22 00:13:13 +0200

    Input and changes
    Changed the word memory to register
    Fixed a problem in the ">" character that prevented append
    Input is complete
    Moved ">" before "<" so that it looks for ocd reasons >.>

--- brainfuck.py
+++ brainfuck.py
@@ -3,49 +3,51 @@ import sys
 #reading file. 
 
 f = open(sys.argv[1], "r")
-code = f.read() #assigning the contents of the file to one string.
+code = f.read() #assigning the contents of the file to one string
 f.close
 
-#empty file error.
+#empty file check
 
 if code == "":
 	print "Error! File is empty!"
 	sys.exit
 
 codePosition = 0
-memory = [0]
-memoryPosition = 0
+register = [0]
+registerPosition = 0
 
 while codePosition < len(code):
-	
-	if code[codePosition] == "<": #pointer move to the left
-		memoryPosition -= 1
-		if memoryPosition < 0: #memory underflow check
-			print "Error! Memory underflow!"
-			sys.exit
 
-	elif code[codePosition] == ">": #pointer move to the right
-		memoryPosition += 1
-		if len(memory) > memoryPosition:
-			memory.append(0)
-		if memoryPosition > 30000: #memory overflow check
-			print "Error! Memory overflow!"
+	if code[codePosition] == ">": #pointer move to the right
+		registerPosition += 1
+		if len(register) <= registerPosition:
+			register.append(0)
+		if registerPosition > 30000: #register overflow check
+			print "Error! Register overflow!"
+			sys.exit
+	
+	elif code[codePosition] == "<": #pointer move to the left
+		registerPosition -= 1
+		if registerPosition < 0: #register underflow check
+			print "Error! Register underflow!"
 			sys.exit
 
 	elif code[codePosition] == "+": #increase value of current cell
-		memory[memoryPosition] += 1
-		if memory[memoryPosition] > 255: #byte overflow, returns to 0
-			memory[memoryPosition] = 0
+		register[registerPosition] += 1
+		if register[registerPosition] > 255: #byte overflow, returns to 0
+			register[registerPosition] = 0
 
 	elif code[codePosition] == "-": #decrease value of current cell
-		memory[memoryPosition] -= 1
-		if memory[memoryPosition] < 0: #byte undeflow returns to 255
-			memory[memoryPosition] = 255
+		register[registerPosition] -= 1
+		if register[registerPosition] < 0: #byte undeflow returns to 255
+			register[registerPosition] = 255
 
 	elif code[codePosition] == ".": #output current cell value in ASCII
-		sys.stdout.write(chr(memory[memoryPosition]))
+		sys.stdout.write(chr(register[registerPosition]))
 
 	elif code[codePosition] == ",": #input current cell ASCII character --> value
+		read = raw_input()
+		register[registerPosition] = ord(read[0])
 
 	elif code[codePosition] == "[":
 
