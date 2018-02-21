8b2643a527ae342694d210c2b5adaea8886933d2
romanos98d <35623989+romanos98d@users.noreply.github.com>
2018-02-22 00:41:40 +0200

    Finished
    completed the last two characters
    polished my comments

--- brainfuck.py
+++ brainfuck.py
@@ -3,10 +3,10 @@ import sys
 #reading file. 
 
 f = open(sys.argv[1], "r")
-code = f.read() #assigning the contents of the file to one string
+code = f.read() #Assigning the contents of the file to one string
 f.close
 
-#empty file check
+#Empty file check
 
 if code == "":
 	print "Error! File is empty!"
@@ -16,41 +16,64 @@ codePosition = 0
 register = [0]
 registerPosition = 0
 
+#Code Execution
+
 while codePosition < len(code):
 
-	if code[codePosition] == ">": #pointer move to the right
+	if code[codePosition] == ">": #Move the pointer to the right
 		registerPosition += 1
 		if len(register) <= registerPosition:
 			register.append(0)
-		if registerPosition > 30000: #register overflow check
+		if registerPosition > 30000: #Register overflow check
 			print "Error! Register overflow!"
 			sys.exit
 	
-	elif code[codePosition] == "<": #pointer move to the left
+	elif code[codePosition] == "<": #Move the pointer to the left
 		registerPosition -= 1
-		if registerPosition < 0: #register underflow check
+		if registerPosition < 0: #Register underflow check
 			print "Error! Register underflow!"
 			sys.exit
 
-	elif code[codePosition] == "+": #increase value of current cell
+	elif code[codePosition] == "+": #Increment the memory cell under the pointer
 		register[registerPosition] += 1
-		if register[registerPosition] > 255: #byte overflow, returns to 0
+		if register[registerPosition] > 255: #Byte overflow, returns to 0
 			register[registerPosition] = 0
 
-	elif code[codePosition] == "-": #decrease value of current cell
+	elif code[codePosition] == "-": #Decrement the memory cell under the pointer
 		register[registerPosition] -= 1
-		if register[registerPosition] < 0: #byte undeflow returns to 255
+		if register[registerPosition] < 0: #Byte undeflow returns to 255
 			register[registerPosition] = 255
 
-	elif code[codePosition] == ".": #output current cell value in ASCII
+	elif code[codePosition] == ".": #Output the character signified by the cell at the pointer
 		sys.stdout.write(chr(register[registerPosition]))
 
-	elif code[codePosition] == ",": #input current cell ASCII character --> value
+	elif code[codePosition] == ",": #Input a character and store it in the cell at the pointer
 		read = raw_input()
 		register[registerPosition] = ord(read[0])
 
-	elif code[codePosition] == "[":
+	elif code[codePosition] == "[": #Jump past the matching ] if the cell under the pointer is 0
+		if register[registerPosition] == 0:
+			brackets = 0
+			codePosition += 1
+			while codePosition < len(code): #Finding the right bracket to close
+				if code[codePosition] == "]" and brackets == 0: 
+					break
+				elif code[codePosition] == "[":
+					brackets += 1
+				elif code[codePosition] == "]":
+					brackets -= 1
+				codePosition +=1
 
-	elif code[codePosition] == "]":
-
+	elif code[codePosition] == "]": #Jump back to the matching [ if the cell under the pointer is nonzero
+		if register[registerPosition] != 0:
+			brackets = 0
+			codePosition -= 1
+			while codePosition < len(code): #Finding the right bracket to continue looping
+				if code[codePosition] == "[" and brackets == 0:
+					break
+				elif code[codePosition] == "]":
+				elif code[codePosition] == "[":
+					brackets -= 1
+				codePosition -=1
+	
 	codePosition += 1
